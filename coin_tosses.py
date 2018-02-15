3acb901fa0fff408e5b9626a240f96e4a5df971c
Daniel <clancy.daniel@gmail.com>
2018-02-15 17:25:45 -0600

    added comments

--- coin_tosses.py
+++ coin_tosses.py
@@ -3,17 +3,23 @@ import random
 
 def coinToss(num):
 	print "Starting the program..."
+	#set the heads and tails to zero
 	heads = 0;
 	tails = 0;
 
-	for x in range(1,num+1): 
+	#loop from one to num
+	for x in range(1,num+1):
+		#start building the output string 
 		message = "Attempt #" + str(x) + ": Throwing a coin... "
+
+		#flip the coin
 		if round(random.random()) == 0: 
 			heads += 1 
 			message += "It's a head!"
 		else: 
 			tails += 1
 			message += "It's a tail!"
+			
 		message += " ... Got " + str(heads) + " head(s) so far and " + str(tails) + " tail(s) so far"
 		print message
 
